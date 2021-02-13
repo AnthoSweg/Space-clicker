a47c12beaa453f1b8985da083fe12c0d68848ff4
Anthony <brossardanthony@outlook.fr>
2021-02-13 16:17:13 +0100

    Fixed camera

---
+++ Assets/_Scripts/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class ExtensionMethods
+{
+
+    public static float Remap(float value, Vector2 from, Vector2 to)
+    {
+        return to.x + (value - from.x) * (to.y - to.x) / (from.y - from.x);
+        //return (value - from.x) / (to.x - from.x) * (to.y - from.y) + from.y;
+    }
+
+}
+
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public int satelliteNbr;
     public GameObject satellitePrefab;
+    [SerializeField]private TextMeshProUGUI satelliteNbrTextMesh;
     // Use this for initialization
     void Start()
     {
@@ -14,15 +16,32 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.S))
-    //        Instantiate(satellitePrefab);
-    //}
+    [SerializeField] private TextMeshProUGUI fpsTextMesh;
+    float fpsUpdateDelay = .5f;
+    float fpsTime = 0f;
+    int FPSCounter = 0;
+    private void Update()
+    {
+        //Display FPS
+        if (fpsTextMesh != null)
+        {
+            FPSCounter++;
+            if (fpsTime + fpsUpdateDelay < Time.unscaledTime)
+            {
+                FPSCounter = Mathf.RoundToInt(1f * FPSCounter / (Time.unscaledTime - fpsTime));
+                fpsTextMesh.text = FPSCounter.ToString();
 
+                fpsTime = Time.unscaledTime;
+                FPSCounter = 0;
+            }
+        }
+    }
+
+    private int satelliteCount;
     public void SpawnSatellite()
     {
         Instantiate(satellitePrefab);
+        satelliteCount++;
+        satelliteNbrTextMesh.text = satelliteCount.ToString();
     }
 }
