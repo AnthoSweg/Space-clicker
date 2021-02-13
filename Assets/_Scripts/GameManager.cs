4f3d287cd8e5e33181e84e9991e304d3d149eff8
Anthony <brossardanthony@outlook.fr>
2021-02-13 13:59:42 +0100

    integration

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -10,14 +10,19 @@ public class GameManager : MonoBehaviour
     {
         for (int i = 0; i < satelliteNbr; i++)
         {
-            Instantiate(satellitePrefab);
+            SpawnSatellite();
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.S))
+    //        Instantiate(satellitePrefab);
+    //}
+
+    public void SpawnSatellite()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-            Instantiate(satellitePrefab);
+        Instantiate(satellitePrefab);
     }
 }
